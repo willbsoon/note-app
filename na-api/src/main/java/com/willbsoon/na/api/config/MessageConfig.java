36d845d8f4828ccea13b4c89b1f218009d8aca5f
bongsoonchoi <willbsoon16@gmail.com>
2021-02-20 20:24:39 +0900

    bug fixed?

--- na-api/src/main/java/com/willbsoon/na/api/config/MessageConfig.java
+++ na-api/src/main/java/com/willbsoon/na/api/config/MessageConfig.java
@@ -5,7 +5,6 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.YamlMapFactoryBean;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
