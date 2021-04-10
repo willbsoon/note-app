f185b3bcb05e8c8b063609162a7612142bc77c37
bongsoonchoi <willbsoon16@gmail.com>
2021-04-11 01:58:37 +0900

    add TestCode

---
+++ na-api/src/test/java/com/willbsoon/na/api/repo/UserJpaRepoTest.java
@@ -0,0 +1,47 @@
+package com.willbsoon.na.api.repo;
+
+import com.willbsoon.na.api.entity.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Collections;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UserJpaRepoTest {
+
+    @Autowired
+    private UserJpaRepo userJpaRepo;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    public void whenFindByUid_thenReturnUser(){
+        String uid = "1234";
+        String name = "1234";
+
+        userJpaRepo.save(User.builder()
+                .uid(uid)
+                .password(passwordEncoder.encode("1234"))
+                .name(name)
+                .roles(Collections.singletonList("ROLE_USER"))
+                .build());
+
+        Optional<User> user = userJpaRepo.findByUid(uid);
+        // then
+        assertNotNull(user);// user객체가 null이 아닌지 체크
+        assertTrue(user.isPresent()); // user객체가 존재여부 true/false 체크
+        assertEquals(user.get().getName(), name); // user객체의 name과 name변수 값이 같은지 체크
+        assertThat(user.get().getName(), is(name)); // user객체의 name과 name변수 값이 같은지 체크
+
+    }
+
+}
