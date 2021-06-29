0d7d69c307dd97ed6ca99b2222b807b820938b91
bongsoonchoi <willbsoon16@gmail.com>
2021-06-30 01:06:23 +0900

    add profile

--- na-api/src/main/java/com/willbsoon/na/api/NaApiApplication.java
+++ na-api/src/main/java/com/willbsoon/na/api/NaApiApplication.java
@@ -1,5 +1,6 @@
 package com.willbsoon.na.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +12,6 @@ import org.springframework.web.client.RestTemplate;
 public class NaApiApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(NaApiApplication.class, args);
 	}
 	
--- na-api/src/test/java/com/willbsoon/na/api/NaApiApplicationTests.java
+++ na-api/src/test/java/com/willbsoon/na/api/NaApiApplicationTests.java
@@ -1,10 +1,19 @@
 package com.willbsoon.na.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NaApiApplicationTests {
+	@Value("${spring.profiles.active}")
+	private String activeProfile;
+
+	@Test
+	void profileTest() {
+		System.out.println("================================================");
+		System.out.println(activeProfile);
+	}
 
 	@Test
 	void contextLoads() {
